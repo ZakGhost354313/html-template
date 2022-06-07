46de3f614f5efead0ccb7def72ba219056b6aaa7
Zak Dahlia <MBM3543@gmail.com>
2022-06-07 09:14:08 -0500

    jkl;

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,5 @@ const app = document.getElementById('app');
 function printHTML(html) { app.innerHTML += html; }
 
 function printText(text) { app.innerText += text; }
+
+function redirect(url) { window.location = url; }
